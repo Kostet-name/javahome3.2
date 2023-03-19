8284a96caaf4596bdef971e34f461c39c0fbf28f
Konstantin <kostenn07@mail.ru>
2023-03-19 21:52:41 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+        public double calculate(double weight, double height) {
+            double index = weight/(height*height);
+            return index;
+        }
+}
